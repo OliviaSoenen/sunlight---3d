b652b411806fb1f62c4878e4d4816da48b4d4adf
OliviaSoenen <73385935+OliviaSoenen@users.noreply.github.com>
2020-10-24 12:14:58 +0200

    I have programmed the micro bit so that when the sunlight is higher than 70 the led lights are all shining, but when the sunlight is less than 70, another pattern appears.

--- main.ts
+++ main.ts
@@ -1,29 +1,20 @@
 basic.forever(function () {
     if (input.lightLevel() > 70) {
         basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
             `)
     }
     if (input.lightLevel() < 70) {
         basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
-    }
-    if (input.lightLevel() < 45) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
             `)
     }
 })
