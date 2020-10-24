6121bcd78182b149dbb49e55e85ae8af5a5d5294
OliviaSoenen <73385935+OliviaSoenen@users.noreply.github.com>
2020-10-24 12:00:49 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+basic.forever(function () {
+    if (input.lightLevel() > 70) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (input.lightLevel() < 70) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
+    if (input.lightLevel() < 45) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
